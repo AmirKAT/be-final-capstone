c6455aaeaccb93312d647d7e8f0eb771b3038a43
Amir <akatal@indeed.com>
2024-02-15 21:21:08 +0100

    add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 if (process.env.NODE_ENV !== 'production') require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Import routers
 const moviesRouter = require("./movies/movies.router");
